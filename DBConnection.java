90247fafe87dbf9b996bc644f2174d3200be30e0
Nguyễn Vịnh <nguyenvinh5722@gmail.com>
2025-12-03 22:45:05 +0700

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class DBConnection {
     private static final String URL="jdbc:mysql://localhost:3306/bankapp";
     private static final String USER="root";
-    private static final String PASS="";
+    private static final String PASS="123456";
 
     public static Connection getConnection(){
         try{
